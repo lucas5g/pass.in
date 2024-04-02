3d411ef89f443877d57fff8a3172d0e7f54c2253
Lucas de Sousa Assunção <lucasdesousa19@hotmail.com>
2024-04-02 19:25:12 +0000

    teste

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,29 @@
+import { PrismaClient } from '@prisma/client'
 import fastify from 'fastify'
+import { z } from 'zod'
+
+const prisma = new PrismaClient({
+	log:['query']
+})
 
 const app = fastify()
+
+app.post('/events', async(req, res ) => {
+	const data = z.object({
+		title: z.string().min(4),
+		details: z.string().nullable(),
+		maximumAttendees: z.number().int().positive().nullable()
+	}).parse(req.body)
+
+	const event = await  prisma.event.create({ 
+		data:{
+			...data, 
+			slug: new Date().toISOString()
+		}
+	})
+	
+	res.status(201).send(event)
+})
 
 app.get('/', () => {
 	return 'Hello nlw unite'
